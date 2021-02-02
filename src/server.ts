721e507d75aba22ec4d266d8c76825916a466a5f
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2021-02-02 01:43:50 -0300

    Express and overnight with controller

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import './util/module-alias';
+import { Server } from '@overnightjs/core';
+import bodyParser from 'body-parser';
+import { ForecastController } from './controllers/forecast';
+
+export class SetupServer extends Server {
+  constructor(private port = 300){
+    super();
+  }
+  public init() : void{
+    this.setupExpress();
+  }
+  private setupExpress(): void{
+    this.app.use(bodyParser.json())
+  }
+  private setupControllers():void{
+    const forecastController = new ForecastController();
+    this.addControllers([forecastController]);
+  }
+}
