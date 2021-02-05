64930d4b4ee1828ee50562d37320dab46ebafb10
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2021-02-05 02:33:40 -0300

    Axios and clients tests

---
+++ src/clients/stormGlass.ts
@@ -0,0 +1,8 @@
+import { AxiosStatic } from 'axios';
+
+export class StormGlass {
+  constructor(protected request: AxiosStatic);
+  public async fetchPoints(lat: number, lng: number): Promise<{}> {
+    return Promise.resolve({});
+  }
+}
